423a46c8b65933aae7d914af62eafa4c9112db23
JoonkiKim <qkjk@DESKTOP-LH9FVAI>
2025-06-19 13:28:51 +0900

    버전테스트두번째

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import EndPage from "../src/components/commons/units/endPage/endPage.container";
 import MainCalendarPage from "../src/components/commons/units/mainCalendar.container";
 
 export default function Home() {
-  console.log("대회종료!! 후 버전관리테스트");
+  console.log("대회종료!! 후 버전관리테스트 한번 더");
   return (
     <>
       <div>
