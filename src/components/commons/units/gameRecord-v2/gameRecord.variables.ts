0fb345ecc503732750ecbe4b5e1e2c7cc64053c4
JoonkiKim <qkjk@DESKTOP-LH9FVAI>
2025-08-01 12:19:47 +0900

    이닝의재구성버튼 성능 향상2

--- src/components/commons/units/gameRecord-v2/gameRecord.variables.ts
+++ src/components/commons/units/gameRecord-v2/gameRecord.variables.ts
@@ -6,14 +6,14 @@ interface BadgeConfig {
 }
 
 export const badgeConfigs: BadgeConfig[] = [
-  { id: "badge-1", label: "이정후", initialLeft: "50%", initialTop: "80%" },
+  { id: "badge-1", label: "이정후", initialLeft: "60%", initialTop: "80%" },
   { id: "badge-2", label: "송성문", initialLeft: "20%", initialTop: "75%" },
   { id: "badge-3", label: "김하성", initialLeft: "20%", initialTop: "85%" },
   { id: "badge-4", label: "박병호", initialLeft: "20%", initialTop: "95%" },
 ];
 
 export const badgeConfigsForModal: BadgeConfig[] = [
-  { id: "badge-1", label: "이정후", initialLeft: "50%", initialTop: "80%" },
+  { id: "badge-1", label: "이정후", initialLeft: "60%", initialTop: "80%" },
   { id: "badge-2", label: "송성문", initialLeft: "20%", initialTop: "75%" },
   { id: "badge-3", label: "김하성", initialLeft: "20%", initialTop: "85%" },
   { id: "badge-4", label: "박병호", initialLeft: "20%", initialTop: "95%" },
